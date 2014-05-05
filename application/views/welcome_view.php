bd148928fb46b7c0300814439b0c553462935675
Ankit <ankitstarski@gmail.com>
2014-05-05 20:37:49 +0530

    ajax controllers for many pages

--- application/views/welcome_view.php
+++ application/views/welcome_view.php
@@ -290,59 +290,44 @@
                                     <style type="text/css">
 										#nitlogo{
 										  background: url('/images/nitsxr.jpg');
-										  width: 90px;
-										  height: 100px;
+										  width: 58px;
+										  height: 64px;
 										  position: relative;
 										  background-size: 100%;
-										  margin-left: 50px;
 										  z-index:1;
 										}
 										#nitlogo div{
 										  color: white;
 										  position: relative;
-										  width:370px;
+										  width:260px;
 										  font-weight: bold;
-										  font:24px 'Georgia','serif';
-										  left: 100px;
-										  top: 9px; 
+										  font:17px 'Georgia','serif';
+										  left: 65px;
+										  top: 3px; 
 										}
 										#nitlogo span{
 										  font-style: italic;
-										  font-size: 20px;
-										}
-										#banner{
-										  position: fixed;
-										  color: white;
-										  font:italic bold 18px 'Georgia','serif';
-										  top:80px;
-										  right: -100px;
-										  -webkit-transform:rotate(45deg);
-										  -moz-transform:rotate(45deg);
-										  -ms-transform:rotate(45deg);
-										  -o-transform:rotate(45deg);
-										  transform:rotate(45deg);
-										  background: red;
-										  padding: 5px 100px;
-										  border: 2px dashed black;
-										  box-shadow: 0 0 0 2px red;
+										  font-size: 14px;
 										}
 										#top-sticky{
 											position:fixed;
 											top:10px;
 											left:100px;
 										}
-										#techv{
-											margin-left:50px;
+										#click-here{
+											background:url(/images/clickhere.png);
+											width:185px;
+											height:34px;
+											margin:10px 0;
+											margin-bottom:110px;
 										}
 									</style>
 									<div id='top-sticky'>
+                                    	<div id="click-here"></div>
 										<div id='nitlogo'>
 										  <div>
 											National Institute of Technology Srinagar<br/> <span>Presents</span>
 										  </div>
 										</div>
 										<img id="techv" src="/images/tv.jpg"/>
-									</div>
-									<div id='banner'>
-									   24 to 26 May, 2014
 									</div>
