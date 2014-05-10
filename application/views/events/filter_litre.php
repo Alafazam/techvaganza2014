d8d2b44056b0798f9bee5516e4b83953c04177ba
alafazam <alafazam@gmail.com>
2014-05-10 12:23:30 +0530

    page buttons

--- application/views/events/filter_litre.php
+++ application/views/events/filter_litre.php
@@ -3,10 +3,25 @@
         <div class="page-header container-fluid">
           <h1 class="center">Filter the Litter</h1>
         </div>
-        	<div  style="float:right;max-width:100px">
-	        <!-- Check with php if user is registered -->
-	            <input id="reg_button"type="submit" name="submit" value="Register">
-	        </div> 
+        	<div  style="float:right;max-width:100px;margin:20px">
+            <?php
+                if($username){
+                    if(!$isRegistered){
+            ?>
+                <a href="/events/<?=$event['category']?>/<?=$event['event_id']?>/register"><input id="reg_button" type="submit" name="submit" value="Register"></a>
+            <?php
+                    }else{
+            ?>
+                <a href="/events/<?=$event['category']?>/<?=$event['event_id']?>/unregister"><input id="unreg_button" type="submit" name="submit" value="Unregister"></a>
+            <?php
+                    }
+                }else{
+            ?>
+                <a href="/login"><input type="submit" value="Register"></a>
+            <?php
+                }
+            ?>        
+        </div>  
 
         	<div class="e_text">
 				<h2>Introduction : </h2>
