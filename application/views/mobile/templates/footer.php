05e3143db4458f9e157ae9fa5e213e70b5286d6f
alafazam <alafazam@gmail.com>
2014-05-14 21:48:11 +0530

    changes in footer , mobile header

--- application/views/mobile/templates/footer.php
+++ application/views/mobile/templates/footer.php
@@ -1,28 +1,27 @@
 	<div class="btn-group btn-group-justified bottom_stiatic">
-  <div class="btn-group">
-    <a  class="btn btn-default" href="/">Home</a>
-  </div>
-  <div class="btn-group">
-    <a  class="btn btn-default" href="/contacts">Contact Us</a>
-  </div>
-  <div class="btn-group">
-    <a class="btn btn-default" 
-<?php 
-	
-	$username= $this->session->userdata('logged_in');
-	$username=$username['username'];
-	if(isset($username))
-	{
-		echo    ' href="/home/logout">Logout';
-	}
-	else
-	{
-		echo    ' href="/login">Login';
-	}
-?>
-      </a>
-  </div>
-</div>
+			<div class="btn-group">
+				<a  class="btn btn-default" href="/">Home</a>
+			</div>
+			<div class="btn-group">
+				<a  class="btn btn-default" href="/contacts">Contact Us</a>
+			</div>
+			<div class="btn-group">
+				<a class="btn btn-default" 
+				<?php 
+					
+					$username= $this->session->userdata('logged_in');
+					$username=$username['username'];
+					if(isset($username))
+					{
+						echo    ' href="/home/logout">Logout';
+					}
+					else
+					{
+						echo    ' href="/login">Login';
+					}
+				?></a>
+  			</div>
+		</div>
 	</div>
 	<script src="/js/jquery.js"></script>
     <script src="/js/bootstrap.min.js"></script>
@@ -32,10 +31,10 @@
 	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 	
-	  ga('create', 'UA-40855347-1', 'techvaganza.org');
-ga('require', 'displayfeatures');
-ga('send', 'pageview');
+		ga('create', 'UA-40855347-1', 'techvaganza.org');
+		ga('require', 'displayfeatures');
+		ga('send', 'pageview');
 	
 	</script>
-	</body>
+</body>
 </html>
